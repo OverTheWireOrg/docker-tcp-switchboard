5a7046c11b38b22c4cd8e71ac417c26d716af49b
Steven Van Acker <steven@singularity.be>
2017-05-26 13:22:10 +0200

    travis uses a weird environment, using docker-in-docker instead

--- travis-ci-test/runtest.sh
+++ travis-ci-test/runtest.sh
@@ -1,5 +1,15 @@
 #!/bin/bash -ex
 
+# start docker, and wait until it's likely up
+/etc/init.d/docker start
+sleep 3
+
+# build echoserv and upperserv
+docker build -t echoserv -f testimages/Dockerfile.echoserv testimages
+docker build -t upperserv -f testimages/Dockerfile.upperserv testimages
+sleep 2
+
+# start the switchboard
 ../docker-tcp-switchboard.py config.ini &
 DAEMONPID=$!
 function cleanup {
@@ -12,4 +22,5 @@ sleep 2 # give time to startup
 
 ./client.py 4 10 7 10
 
-
+# Show logfile
+cat /tmp/logfile
