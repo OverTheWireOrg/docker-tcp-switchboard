3ada5e38c58910518c4e069e117b7d2290c8e3f2
Steven Van Acker <steven@singularity.be>
2017-05-29 08:31:26 +0200

    on second thought, even 1.5 minutes is more than enough time

--- travis-ci-test/runtest_basic.sh
+++ travis-ci-test/runtest_basic.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ex
 
 # start the switchboard
-timeout --signal=KILL 300 ../docker-tcp-switchboard.py config.ini &
+timeout --signal=KILL 90 ../docker-tcp-switchboard.py config.ini &
 DAEMONPID=$!
 function cleanup {
   echo "Cleaning up..."
