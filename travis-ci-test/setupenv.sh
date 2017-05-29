e19e0a2a43b44628bbc42ebbd66474f9dd5d0453
Steven Van Acker <steven@singularity.be>
2017-05-29 09:39:48 +0200

    switching travis to python language to maybe fix pip problems

--- travis-ci-test/setupenv.sh
+++ travis-ci-test/setupenv.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -ex
 
 # installing some packages before docker install maybe messes things up
-sudo apt-get update
-apt-cache search python3-pip || true
-apt-cache search pip3 || true
-sudo apt-get install -y python3-pip
-sudo pip3 install -r ../requirements.txt
+#sudo apt-get update
+#apt-cache search python3-pip || true
+#apt-cache search pip3 || true
+#sudo apt-get install -y python3-pip
+sudo pip install -r ../requirements.txt
 
 # install latest docker if it doesn't already exist
 if [ ! -e /etc/init.d/docker ]; then
